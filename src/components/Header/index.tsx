eef0a73e7fa121e0bf4ca5f6473ec71089b7ca38
Junior Fortunato <fortunatojunior2020@gmail.com>
2022-10-06 18:14:39 -0300

    navbar

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,29 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import logo from '../../../public/images/logo.png'
+import styles from './styles.module.scss'
+
+export function Header() {
+    return (
+        <header className={styles.container}>
+            <div className={styles.content}>
+                <nav>
+                    <Link href={'#'}><a>Trends</a></Link>
+                    <Link href={'#'}><a>Trends</a></Link>
+                    <Link href={'#'}><a>Trends</a></Link>
+                    <Link href={'#'}>
+                        <a>
+                            <Image className={styles.logo} src={logo} alt='logo-movies-review' />
+                        </a>
+                    </Link>
+                    <Link href={'#'}><a>Trends</a></Link>
+                    <Link href={'#'}><a>Trends</a></Link>
+                    <Link href={'#'}><a>Trends</a></Link>
+                </nav>
+                <div>
+                    sair
+                </div>
+            </div>
+        </header>
+    )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,10 @@
+import { Header } from '../components/Header'
 import styles from '../styles/home.module.scss'
 
 export default function Home() {
     return (
-        <div>
-            home
-        </div>
+        <>
+            <Header />
+        </>
     )
 }
